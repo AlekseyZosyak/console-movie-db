86217bc7853b54bd3c38501381a2e806b3c64c10
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2024-09-02 09:47:06 +0300

    dz2

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ const personalMuvieDB = {
     privat : false
 }
 
-const a = prompt('Последний из просмотренных фильмов?');
-const b = prompt('На сколько оцените его?');
+if (personalMuvieDB.count < 10) {
+    console.log('Слишком мало фильмов...');
+} else if (personalMuvieDB.count >= 10 && personalMuvieDB.count <= 30) {
+    console.log('Средний пользователь.');
+} else if (personalMuvieDB.count >= 30) { 
+    console.log('Вы киноман!');
+} else {
+    console.log(`Error : Count`);
+}
 
-personalMuvieDB.movies[a] = b;
-
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Последний из просмотренных фильмов?');
+    const b = prompt('На сколько оцените его?');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMuvieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+// написать еще два варианта цикла
 console.log(personalMuvieDB);
