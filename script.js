dddf2236f55cea7c93891537fe0bdfc56e0de8c9
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2024-09-04 21:11:05 +0300

    4

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = Number(prompt('сколько фильмов вы смотрели?'));
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = Number(prompt('сколько фильмов вы смотрели?'));
-    }
-}
-
-start();
-
 const personalMuvieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Последний из просмотренных фильмов?');
-        const b = prompt('На сколько оцените его?');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMuvieDB.movies[a] = b;
-            console.log('Done');
-        } else {
-            console.log('Error');
-            i--;
+    privat: false,
+    start: function() {
+        const {count} = personalMuvieDB;
+        count = Number(prompt('сколько фильмов вы смотрели?').trim());
+    
+        while (count == '' || count == null || isNaN(count)) {
+            count = Number(prompt('сколько фильмов вы смотрели?'));
         }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Последний из просмотренных фильмов?').trim();
+            const b = prompt('На сколько оцените его?').trim();
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMuvieDB.movies[a] = b;
+                console.log('Done');
+            } else {
+                console.log('Error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMuvieDB.count < 10) {
+            console.log('Слишком мало фильмов...');
+        } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count <= 30) {
+            console.log('Средний пользователь.');
+        } else if (personalMuvieDB.count >= 30) {
+            console.log('Вы киноман!');
+        } else {
+            console.log(`Error : Count`);
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMuvieDB);
+        } else {
+            console.log('Главный объект программы');
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMuvieDB.privat) {
+            personalMuvieDB.privat = false;
+        } else {
+            personalMuvieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            const genre = prompt(`Ваш любимый жанр под номером ${i + 1}`)
+
+            if (genre === '' || genre == null) {
+                console.log("Вы не ввели данные");
+                i--;
+            } else {
+                personalMuvieDB.genres[i] = genre;
+            }
+        }
+        personalMuvieDB.genres.forEach((item, i) => {
+            console.log(`Под ${i + 1} номером любимый жанр: ${item}`)
+        })
     }
 }
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMuvieDB.count < 10) {
-        console.log('Слишком мало фильмов...');
-    } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count <= 30) {
-        console.log('Средний пользователь.');
-    } else if (personalMuvieDB.count >= 30) {
-        console.log('Вы киноман!');
-    } else {
-        console.log(`Error : Count`);
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMuvieDB);
-    } else {
-        console.log('Главный объект программы');
-    }
-}
-showMyDB(personalMuvieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i + 1}`)
-        personalMuvieDB.genres[i] = genre;
-    }
-}
-
-writeYourGenres();
-
-console.log(personalMuvieDB);
-
